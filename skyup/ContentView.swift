66d3c135347fc34b5063994ab4a054e234dba380
Martin Gressler <martin.gressler@code.berlin>
2024-09-03 13:18:40 +0200

    Initial Commit

---
+++ skyup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  skyup
+//
+//  Created by Martin Gressler on 03.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ skyup/skyupApp.swift
@@ -0,0 +1,17 @@
+//
+//  skyupApp.swift
+//  skyup
+//
+//  Created by Martin Gressler on 03.09.24.
+//
+
+import SwiftUI
+
+@main
+struct skyupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
